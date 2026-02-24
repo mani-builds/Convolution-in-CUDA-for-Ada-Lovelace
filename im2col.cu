ae8b6dca99bfa9b8740930c65c9409ef1d29a483
mani-builds <manikantatjea973@gmail.com>
2026-02-24 14:17:14 -0800

    working im2col.cu

--- im2col.cu
+++ im2col.cu
@@ -7,9 +7,10 @@
 
 // __device__ float im2col[im2col_H][im2col_W];
 
-#define IDX(i,j,width) ((i)*(width) + (j))
+#define IDX2R(i,j,width) ((i)*(width) + (j))
+#define IDX2C(i,j,width) ((j)*(width) + (i))
 
-__global__ void im2col_kernel(float *image,int H_in, int W_in, int kernel_size, float *im2col){
+__global__ void im2col_kernel(float *image,int H_in, int W_in, int im2col_H, int im2col_W, int kernel_size, float *im2col){
   int row = blockIdx.y * blockDim.y + threadIdx.y;
   int col = blockIdx.x * blockDim.x + threadIdx.x;
 
@@ -17,17 +18,18 @@ __global__ void im2col_kernel(float *image,int H_in, int W_in, int kernel_size, 
   // 9 is the patch size and 25 is number of patches
   // Each thread could create a patch, so in total there are 25 patches = 5x5
 
+  // int patch_idx = out_row * im2col_W + out_col;
   int idx_count = 0;
-  if (row < H_in && col < W_in) {
 
+  if (row < H_in && col < W_in) {
     for (int i = -FILTER_RADIUS; i <= FILTER_RADIUS; i++) {
       for (int j = -FILTER_RADIUS; j <= FILTER_RADIUS; j++) {
-        if (col + i < 0 || col + i > W_in || row + j < 0 || row + j > H_in) {
-          im2col[IDX(idx_count,row*W_in + col, W_in*H_in)] = 0.0f;
-        } else {
-          im2col[IDX(idx_count,row*W_in + col, W_in*H_in)] = image[(row+j) * W_in + (col+j)];
+        float val = 0.0f;
+        if (col + j >= 0 && col + j < W_in && row + i >= 0 && row + i < H_in) {
+          val  = image[(row+i) * W_in + (col+j)];
         }
-
+        im2col[IDX2R(idx_count, row * W_in + col, im2col_W)] = val;
+        idx_count++;
       }
     }
   }
@@ -66,11 +68,11 @@ int main(){
 
   printf("\nInput image: \n");
   for (int i = 0; i < H_in * W_in; i++) {
-    printf("%.2f ", image_h[i]);
+    printf("%.1f ", image_h[i]);
   }
   printf("\nKernel: \n");
   for (int i = 0; i < kernel_size * kernel_size; i++) {
-    printf("%.2f ", kernel_h[i]);
+    printf("%.1f ", kernel_h[i]);
   }
 
   H_out = output_dim(H_in, PADDING, kernel_size, STRIDE);
@@ -91,22 +93,24 @@ int main(){
   cudaMemcpy(image,image_h,C_in*H_in*W_in*sizeof(float), cudaMemcpyHostToDevice);
   cudaMemcpy(kernel,kernel_h,C_in*kernel_size*kernel_size*sizeof(float), cudaMemcpyHostToDevice);
 
-  // printf("Input DIM: (%d, %d)\n",H_in, W_in);
-  // printf("im2col DIM: (%d, %d)\n",im2col_H, im2col_W);
+   printf("Input DIM H_in and W_in: (%d, %d)\n",H_in, W_in);
+  printf("im2col DIM im2col_H and im2col_W: (%d, %d)\n",im2col_H, im2col_W);
 
   dim3 threadsPerBlock(H_in, W_in);
   dim3 blocksPerGrid((H_in + threadsPerBlock.x - 1) / threadsPerBlock.x,
                      (W_in + threadsPerBlock.y - 1) / threadsPerBlock.y);
 
-  im2col_kernel<<<blocksPerGrid,threadsPerBlock>>>(image, H_in, W_in, kernel_size, im2col);
+  im2col_kernel<<<blocksPerGrid,threadsPerBlock>>>(image, H_in, W_in, im2col_H, im2col_W, kernel_size, im2col);
 
-  cudaMemcpy(im2col_h, im2col, C_in*kernel_size*kernel_size*sizeof(float), cudaMemcpyDeviceToHost);
+  cudaMemcpy(im2col_h, im2col, im2col_H*im2col_W*sizeof(float), cudaMemcpyDeviceToHost);
 
   printf("\nIm2col image: \n");
-  for (int i = 0; i < im2col_H * im2col_W; i++) {
-    printf("%.2f ", im2col_h[i]);
+  for (int i = 0; i < im2col_H ; i++) {
+  for (int j = 0; j < im2col_W ; j++) {
+    printf("%.1f ", im2col_h[IDX2R(i, j, im2col_W)]);
   }
   printf("\n");
+  }
 
   free(image_h);
   free(kernel_h);
